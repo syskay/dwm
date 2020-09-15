bed0c9812bf29594cb3608fc5dbf2023dd5197b6
syskay <syskay@gmail.com>
2020-09-16 00:42:52 +0200

    renamed config.def.h to config.h and enable emoji support with libxft-bgra

--- config.h
+++ config.h
@@ -5,7 +5,9 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "Hack:pixelsize=12:antialias=true:autohint=true",
+	 				"JoyPixels:pixelsize=8:antialias=true:autohint=true",
+};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -17,9 +19,10 @@ static const char *colors[][3]      = {
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
-
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "💻", "⚒", "🔉", "🔒", "🌍" };
+//static const char *tagsalt[] = { "1", "2", "3", "4", "5" };
+
 
 static const Rule rules[] = {
 	/* xprop(1):
